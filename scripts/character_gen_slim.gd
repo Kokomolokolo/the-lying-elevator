fe2c4a40df99451098f1fba45404cbf5c098bc91
Lenni707 <gamer_like_you@gmx.de>
2026-08-26 22:35:15 +0200

    added working randomization for hoodies and tshirts

--- scripts/character_gen_slim.gd
+++ scripts/character_gen_slim.gd
@@ -3,6 +3,7 @@ extends Node2D
 @export var textures: Dictionary = {
 	"body_slim": preload("res://assets/pain_human.png"),
 	"tshirt": preload("res://assets/white_shirt.png"),
+	"hoodie": preload("res://assets/white_hoodie.png")
 }
 
 @export var appearance_data: Dictionary = {
@@ -28,37 +29,41 @@ extends Node2D
 func rand_color() -> Color:
 	return colors[randi_range(0, len(colors) - 1)]
 
-func rand_tshirt() -> void: 
-	var color = rand_color()
+func rand_tshirt_or_hoodie(type: String) -> void:
+	var chosen_color = rand_color()
 	
-	appearance_data["tshirt_color"] = color
-	appearance_data["tshirt_color"] = textures["tshirt"]
+	var color_key: String = type + "_color"
+	var texture_key: String = type + "_texture"
 	
-	tshirt.texture = appearance_data["tshirt_texture"]
-	tshirt.modulate = appearance_data["tshirt_color"]
+	appearance_data[color_key] = chosen_color
+	appearance_data[texture_key] = textures[type]
 	
-func rand_top() -> void:
-	var roll: float = randf() # Returns a random float between 0.0 and 1.0
+	var target_sprite: Sprite2D = hoodie if type == "hoodie" else tshirt
+	target_sprite.texture = appearance_data[texture_key]
+	target_sprite.modulate = appearance_data[color_key]
 
-	if roll < 0.10:
+
+func rand_top() -> void:
+	var roll: float = randf()
+
+	if roll < 0.10: # 10% nackt
 		appearance_data["top"] = "none"
-		tshirt.visible = false # mache ich nur, weil im editor er ein tshirt anhaben muss wegen positionieren und so
+		tshirt.visible = false
 		hoodie.visible = false
-	elif roll < 0.55: # hoodie
-		
+	elif roll < 0.55: # 45% hoodie
+		appearance_data["top"] = "hoodie"
+		rand_tshirt_or_hoodie("hoodie")
 		hoodie.visible = true
 		tshirt.visible = false
-	else: # thsirt
-		rand_tshirt()
+	else: # 45% tshirt
+		appearance_data["top"] = "tshirt"
+		rand_tshirt_or_hoodie("tshirt")
 		hoodie.visible = false
 		tshirt.visible = true
 
 func randomize_character() -> void:
 	rand_top()
-		
 	base_body.texture = appearance_data["body"]
-	
-	
 
 # zum trollen
 func _on_doors_animation_finished() -> void:
