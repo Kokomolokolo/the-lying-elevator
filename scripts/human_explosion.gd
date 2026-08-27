6d9f845867b4697f44f1370b9f6f4a2314a3a2b2
Kokomolokolo <kokoloko.gaming@gmail.com>
2026-08-27 18:01:49 +0200

    d

--- scripts/human_explosion.gd
+++ scripts/human_explosion.gd
@@ -1,9 +1,13 @@
 extends AnimatedSprite2D
 
+@onready var gun_sound: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	gun_sound.play()
 	self.play()
+	
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
